91a63367442286ceeed493a484a86c79458d40f4
Ryandi Pratama Purba <purbaryandi@gmail.com>
2020-02-11 12:51:33 +0700

    create component header, podcast,detail podcast, search box

---
+++ src/components/DetailPodcasts.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DetailPodcasts = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default DetailPodcasts;
---
+++ src/components/SearchBox.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import { Router } from "@reach/router";
+import axios from 'axios';
+
+const SearchBox = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default SearchBox;
