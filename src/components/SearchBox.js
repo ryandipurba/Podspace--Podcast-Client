f4fc1ca8b3c49ad8c36cea79b0fd9158346281d6
Ryandi Pratama Purba <purbaryandi@gmail.com>
2020-02-11 13:03:44 +0700

    membuat form search

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Button, Row, Col } from 'react-bootstrap';
 
-const SearchBox = () => {
+const SearchBox = (props) => {
     return (
-        <div>
-
+        <div className="search-bar">
+            <form>
+                <Row>
+                    <Col md={11}>
+                        <input type="text" className="form-control" placeholder="search" onChange={props.handleSearchInput} />
+                    </Col>
+                    <Button onClick={props.handleButtonSearch} type="button" variant="outline-success" >Search</Button>
+                </Row>
+            </form>
         </div>
     );
 };
